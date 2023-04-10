6e451530cd8e7d8e76b88e5c8061a9554e4856e1
djay64 <57293939+djay64@users.noreply.github.com>
2023-04-10 13:57:43 +0200

    ajout de la pomme

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ window.onload = function()
     var ctx; 
     var delay = 1000; 
     var snakee; 
+    var applee; 
 
     init(); 
 
@@ -19,6 +20,7 @@ function init()
     document.body.appendChild(canvas) ; 
     ctx = canvas.getContext('2d');
     snakee = new Snake([[6,4],[5,4],[4,4]],"right"); 
+    applee = new Apple([10,10]);
     refreshCanvas() ; 
     }
 
@@ -27,6 +29,7 @@ function refreshCanvas()
     ctx.clearRect(0,0,canvasWidth, canvasHeight) ;
 snakee.draw(); 
 snakee.advance() ; 
+applee.draw(); 
 setTimeout(refreshCanvas,delay) ; 
 
 }
@@ -102,6 +105,31 @@ this.direction = newDirection ;
     } ; 
 
 }
+
+function Apple(position) 
+
+{
+this.position = position ; 
+this.draw = function()
+{
+    ctx.save() ;
+    ctx.fillStyle = "#33cc33" ; 
+    ctx.beginPath() ; 
+    var radius = blockSize/2 ; 
+    var x = position[0]+blockSize+radius ; 
+    var y = position[1]+blockSize+radius ; 
+    ctx.arc(x,y, radius,0, Math.PI*2, true) ;
+    ctx.fill(); 
+
+    ctx.restore() ;
+} ; 
+
+}
+
+
+
+
+
 
 document.onkeydown = function handleKeyDown(e)
 {
